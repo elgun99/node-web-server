f2c0b26bad26322d9be3cc5197b77ab9c2f392e5
Elgun <elgun1999@gmail.com>
2018-08-15 17:23:19 +0400

    Added Projects Page

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  })
+})
+
 app.listen(port, () => {
   console.log('Server is up');
 });
